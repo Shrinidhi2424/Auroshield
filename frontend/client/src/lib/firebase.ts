3ff118c5312bf331e3861e28992590bd44bfc314
Shrinidhi Shetty <shrinidhi.s.s1234@gmail.com>
2025-09-20 20:26:31 +0530

    Day 3

--- frontend/client/src/lib/firebase.ts
+++ frontend/client/src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "WRONG-API-KEY-123456",
-  authDomain: "wrong-project.firebaseapp.com",
-  projectId: "wrong-project-id",
-  storageBucket: "wrong-bucket.appspot.com",
-  messagingSenderId: "999999999999",
-  appId: "1:999999999999:web:wrongappid",
-  measurementId: "G-WRONG1234",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
